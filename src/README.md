c1c7638fb832d83080e233a8dc254188dd5abf4b
cyd <email@1819949402@qq.com>
2019-11-08 17:22:45 +0800

    today

--- src/README.md
+++ src/README.md
@@ -1,2 +1,13 @@
 # 这是第一个Vue项目
-## kjjjjjjjj;l
+## 制作首页App组件
+1. 完成header区域，使用mint-ui的header组件
+2. 制作底部的tab bar区域，mui
++ 在制作购物车图标时要拷贝扩展图标和拓展字体添加到项目中
+3. 中间的路由区域放置router-view来展示路由匹配到的组件
+
+## 改造tabbar为 router-link
+
+## 设置路由高亮
+路由激活对应的组件时可通过 `linkActiveClass`覆盖默认组件样式
+
+## 点击tabbar中的组件链接，展示对应组件
--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 // 入口文件
 import Vue from 'vue'
+
+
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+import router from './router.js'
+
 import app from './App.vue'
 import { Header } from 'mint-ui'
 import './lib/mui/css/mui.min.css'
-
+import './lib/mui/css/icons-extra.css'
 Vue.component(Header.name,Header)
 
 var vm = new Vue({
     el: '#app',
     render: c=>c(app),
+    router
 })
